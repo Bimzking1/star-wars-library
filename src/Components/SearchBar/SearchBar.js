99a731369cf73aa95f3729c0c33fc1d60bf1be96
Bimzking1 <kingofbimo@gmail.com>
2022-04-06 01:16:58 +0700

    Async Part 3 - Setup Form Fixes #5

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,40 +1,111 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import style from "./SearchBar.module.css"
 
 const SearchBar = () => {
-  return (
-    <div>
-        <form id="searc-movie" action="/">
-            <div className={style.search_container}>
-                <div className={style.search_group}>
-                    <label for="character">Character</label>
-                    <select name="character" id="character">
-                        <option value="">-- Select Character --</option>
-                    </select>
+
+    const [characterOptions, setCharacterOptions] = useState(['option1', 'option2'])
+    const [planetOptions, setPlanetOptions] = useState(['option1', 'option2'])
+    const [speciesOptions, setSpeciesOptions] = useState(['option1', 'option2'])
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people')
+        .then( response => {
+            return response.json()
+        })
+        .then( data => {
+            setCharacterOptions([...data.results])
+        })
+        .catch( error => {
+            console.log(error)
+        })
+    },[])
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/planets')
+        .then( response => {
+            return response.json()
+        })
+        .then( data => {
+            setPlanetOptions([...data.results])
+        })
+        .catch( error => {
+            console.log(error)
+        })
+    },[])
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/species')
+        .then( response => {
+            return response.json()
+        })
+        .then( data => {
+            setSpeciesOptions([...data.results])
+        })
+        .catch( error => {
+            console.log(error)
+        })
+    },[])
+
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+    return (
+        <div>
+            <form id="searc-movie" onSubmit={handleSubmit(onSubmit)}>
+                <div className={style.search_container}>
+                    <div className={style.search_group}>
+                        <label htmlFor="character">Character</label>
+                        <select name="character" id="character" {...register('character')}>
+                            <option value="">-- Select Character --</option>
+                            {characterOptions.map((item, index) => {
+                                return (
+                                    <option value={item.name} key={index}>
+                                        {item.name}
+                                    </option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                    <div className={style.search_group}>
+                        <label htmlFor="planet">Planet</label>
+                        <select name="planet" id="planet" {...register('planet')}>
+                            <option value="">-- Select Planet --</option>
+                            {planetOptions.map((item, index) => {
+                                return (
+                                    <option value={item.name} key={index}>
+                                        {item.name}
+                                    </option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                    <div className={style.search_group}>
+                        <label htmlFor="species">Species</label>
+                        <select name="species" id="species" {...register('species')}>
+                            <option value="">-- Select Species --</option>
+                            {speciesOptions.map((item, index) => {
+                                return (
+                                    <option value={item.name} key={index}>
+                                        {item.name}
+                                    </option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                    <div className={style.search_group}>
+                        <label htmlFor="release_date">Release Data After</label>
+                        <input type="date" name="release_date" id="release_date" {...register('release_date')}/>
+                    </div>
+                    <div className={style.submit}>
+                        <button type="submit">Search</button>
+                    </div>
                 </div>
-                <div className={style.search_group}>
-                    <label for="planet">Planet</label>
-                    <select name="planet" id="planet">
-                        <option value="">-- Select Planet --</option>
-                    </select>
-                </div>
-                <div className={style.search_group}>
-                    <label for="species">Species</label>
-                    <select name="species" id="species">
-                        <option value="">-- Select Species --</option>
-                    </select>
-                </div>
-                <div className={style.search_group}>
-                    <label for="release_date">Release Data After</label>
-                    <input type="date" name="release_date" id="release_date"/>
-                </div>
-                <div className={style.submit}>
-                    <button type="submit">Search</button>
-                </div>
-            </div>
-        </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar
